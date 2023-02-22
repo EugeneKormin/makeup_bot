d8749d0b4844fd68a28add0729a73422f6af6037
eKormin <resolweru@mail.ru>
2023-02-22 23:04:56 +1000

    add the rest products

--- API.py
+++ API.py
@@ -5,31 +5,51 @@ import pandas as pd
 
 class API(object):
     def __init__(self):
-        self.lipstick_category_list = [
-            "lipstick",
-            "lip gloss",
-            "liquid",
-            "lip stain",
-            "all"
+        self.lipstick_category_list = ["lipstick", "lip gloss", "liquid", "lip stain", "all"
         ]
 
-        self.lipstick_tag_list = [
-            "canadian",
-            "natural",
-            "gluten free",
-            "non gmo",
-            "peanut free product",
-            "vegan",
-            "cruelty free",
-            "organic",
-            "purpicks",
-            "certclean",
-            "chemical free",
-            "ewg verified",
-            "hypoallergenic",
-            "no talc",
-            "all"
+        self.lipstick_tag_list = ["canadian", "natural", "gluten free", "non gmo", "peanut free product", "vegan",
+            "cruelty free", "organic", "purpicks", "certclean", "chemical free", "ewg verified", "hypoallergenic",
+            "no talc", "all"
         ]
+
+        self.blush_category_list = ["powder", "cream", "all"]
+
+        self.blush_tag_list = ["Vegan", "Gluten free", "Canadian", "Natural", "Non-gmot", "Purpicks", "Usda organic",
+            "Organic", "Certclean", "Ewg verified", "Hypoallergenic", "No talc",
+        ]
+
+        self.bronzer_category_list = ["powder", "all"]
+        self.bronzer_tag_list = ["gluten_free", "canadian", "natural", "organic", "vegan", "purpicks", "ewg_verified", "all"]
+
+        self.eyebrow_category_list = ["pencil", "all"]
+        self.eyebrow_tag_list = ["Ewg verified", "Purpicks", "all"]
+
+        self.eyeliner_category_list = ["Liquid", "Pencil", "Gel", "Cream", "all"]
+        self.eyeliner_tag_list = ["Vegan", "Natural", "Canadian", "Gluten free", "Organic", "Purpicks", "Certclean",
+                             "Ewg verified", "Hypoallergenic", "No talc", "Ecocert", "all"]
+
+        self.eyeshadow_category_list = ["Palette", "Pencil", "Cream", "All"]
+        self.eyeshadow_tag_list = ["Vegan", "Canadian", "Natural", "Gluten Free", "Non-GMO", "Purpicks", "CertClean",
+                              "EWG Verified", "Organic", "USDA Organic", "Hypoallergenic", "No Talc", "EcoCert", "All"]
+
+        self.foundation_category_list = ["Concealer", "Liquid", "Contour", "Bb cc", "Cream", "Mineral", "Powder",
+                                         "Highlighter", "All"]
+        self.foundation_tag_list = ["Vegan", "Canadian", "Natural", "Gluten Free", "Purpicks", "Certclean",
+                                    "Ewg Verified", "Hypoallergenic", "No Talc", "Water Free", "Cruelty Free",
+                                    "Alcohol Free", "Oil Free", "Silicone Free", "All"]
+
+        self.lipliner_category_list = ["pencil", "all"]
+        self.lipliner_tag_list = ["Natural", "Vegan", " Gluten free", "Canadian", "Purpicks", "Ewg verified",
+                                  "Hypoallergenic", "No talc", "Cruelty free", "All"]
+
+        self.mascara_category_list = ["all"]
+        self.mascara_tag_list = ['Natural', 'Gluten Free', 'Vegan', 'Canadian', 'Organic', 'Purpicks', 'Ewg Verified',
+                            'Hypoallergenic', 'No Talc', 'Ecocert', 'Usda Organic', 'Certclean', 'All']
+
+        self.nailpolish_category_list = ["all"]
+        self.nailpolish_tag_list = ["Vegan", "Canadian", "Natural", "Gluten free", "Fair trade", "Sugar free",
+                                    "Non-gmo", "Dairy free", "All"]
 
         self.__product_type = ""
         self.__brand = ""
@@ -40,6 +60,52 @@ class API(object):
         if TYPE == "lipstick":
             CHECK_CATEGORY = True if CATEGORY in self.lipstick_category_list else False
             CHECK_TAG = True if TAG in self.lipstick_tag_list else False
+
+        # Blush
+        if TYPE == "blush":
+            CHECK_CATEGORY = True if CATEGORY in self.blush_category_list else False
+            CHECK_TAG = True if TAG in self.blush_tag_list else False
+
+        # Bronzer
+        if TYPE == "bronzer":
+            CHECK_CATEGORY = True if CATEGORY in self.bronzer_category_list else False
+            CHECK_TAG = True if TAG in self.bronzer_tag_list else False
+
+        # Eyebrow
+        if TYPE == "eyebrow":
+            CHECK_CATEGORY = True if CATEGORY in self.eyebrow_category_list else False
+            CHECK_TAG = True if TAG in self.eyebrow_tag_list else False
+
+        # Eyeliner
+        if TYPE == "eyeliner":
+            CHECK_CATEGORY = True if CATEGORY in self.eyeliner_category_list else False
+            CHECK_TAG = True if TAG in self.eyeliner_tag_list else False
+
+        # Eyeshadow
+        if TYPE == "eyeshadow":
+            CHECK_CATEGORY = True if CATEGORY in self.eyeshadow_category_list else False
+            CHECK_TAG = True if TAG in self.eyeshadow_tag_list else False
+
+        # Foundation
+        if TYPE == "foundation":
+            CHECK_CATEGORY = True if CATEGORY in self.foundation_category_list else False
+            CHECK_TAG = True if TAG in self.foundation_tag_list else False
+
+        # Lipliner
+        if TYPE == "lipliner":
+            CHECK_CATEGORY = True if CATEGORY in self.lipliner_category_list else False
+            CHECK_TAG = True if TAG in self.lipliner_tag_list else False
+
+        # Mascara
+        if TYPE == "mascara":
+            CHECK_CATEGORY = True if CATEGORY in self.mascara_category_list else False
+            CHECK_TAG = True if TAG in self.mascara_tag_list else False
+
+        # Nailpolish
+        if TYPE == "nailpolish":
+            CHECK_CATEGORY = True if CATEGORY in self.nailpolish_category_list else False
+            CHECK_TAG = True if TAG in self.nailpolish_tag_list else False
+
         return CHECK_CATEGORY, CHECK_TAG
 
     @staticmethod
